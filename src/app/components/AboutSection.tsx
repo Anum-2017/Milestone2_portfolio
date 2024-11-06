07bbbac23c56130fe8bb671c646040a34e6b6a2b
Anum Kamal (Web Developer) <87061778+Anum-2017@users.noreply.github.com>
2024-11-07 00:56:05 +0500

    Update AboutSection.tsx

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 
-const AboutSection: React.FC = () => {
+const AboutSection = () => {
   return (
     <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
