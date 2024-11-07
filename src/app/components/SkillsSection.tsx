772a5e4fead118ae599d69f52196b89b796ef429
Anum Kamal (Web Developer) <87061778+Anum-2017@users.noreply.github.com>
2024-11-07 16:43:20 +0500

    Update SkillsSection.tsx

--- src/app/components/SkillsSection.tsx
+++ src/app/components/SkillsSection.tsx
@@ -15,9 +15,9 @@ const SkillsSection = () => {
   ];
 
   return (
-    <section id="skills" className=" text-gray-200 py-16 px-4">
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="flex flex-col font-serif text-center w-full mb-20">
+    <section id="skills" className="py-16">
+      <div className="max-w-7xl mx-auto px-6">
+      <div className="flex flex-col font-serif text-center w-full mb-20">
             <h1 className="sm:text-5xl text-5xl font-serif font-bold title-font text-white">
               My
               <span className="text-5xl text-white sm:text-5xl font-serif font-bold ml-[15px]">
@@ -25,24 +25,29 @@ const SkillsSection = () => {
               </span>
             </h1>
           </div>
-
-        <div className="flex flex-wrap gap-6 justify-between">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {skills.map((skill, index) => (
-            <div className="p-8 rounded-lg flex flex-col justify-between flex-1 min-w-[280px] max-w-[300px] transition-transform transform hover:scale-105 hover:shadow-lg" key={index}>
-              <div className="flex items-center mb-5">
-                <div className="w-10 h-10 bg-pink-500 rounded-full flex justify-center items-center mr-4">
-                  <IoMdCheckboxOutline className="text-white text-xl" />
+            <div
+              key={index}
+              className="shadow-lg rounded-lg p-6 flex flex-col"
+            >
+              <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <div className="w-8 h-8 flex items-center justify-center bg-pink-500 text-white rounded-full">
+                      <IoMdCheckboxOutline className="text-2xl" />
+                    </div>
+                  </div>
+                  <h2 className="ml-3 text-xl font-semibold text-white">{skill.name}</h2>
                 </div>
-                <h2 className="text-xl font-semibold text-white">{skill.name}</h2>
+                <p className="text-sm font-medium text-pink-500">{skill.progress}%</p>
               </div>
-              <div className="flex justify-between items-center mt-4">
-                <div className="w-full h-2 bg-gray-400 rounded-full relative">
-                  <div
-                    className="bg-pink-500 h-full rounded-full"
-                    style={{ width: `${skill.progress}%` }}
-                  ></div>
-                </div>
-                <p className="font-bold text-pink-500 ml-4">{skill.progress}%</p>
+
+              <div className="relative w-full bg-gray-200 rounded-full h-2.5">
+                <div
+                  className="bg-pink-500 h-2.5 rounded-full"
+                  style={{ width: `${skill.progress}%` }}
+                ></div>
               </div>
             </div>
           ))}
